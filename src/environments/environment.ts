90f891009b398a18d864e399dafdadb461139f8c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-09-15 09:20:18 +0100

    refactor(env): Moved API to enviroment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
+  apiUrl: 'https://connect.core.nxfifteen.me.uk',
   production: false
 };
