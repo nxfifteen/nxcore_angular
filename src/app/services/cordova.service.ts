057bac14dd94cc13347faed56857c318bc6863b4
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-10-15 13:04:16 +0100

    feat(cordova): Removed device usage

--- src/app/services/cordova.service.ts
+++ src/app/services/cordova.service.ts
@@ -3,7 +3,6 @@ import {BehaviorSubject, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {AppVersion} from '../_models/appVersion';
-import {CordovaDevice} from '../_models/cordovaDevice';
 
 function _window(): any {
   // return the global native browser window object
@@ -18,9 +17,6 @@ export class CordovaService {
   private updateAvailableSubject: BehaviorSubject<AppVersion>;
   public updateAvailable: Observable<AppVersion>;
 
-  private cordovaDeviceSubject: BehaviorSubject<CordovaDevice>;
-  public cordovaDevice: Observable<CordovaDevice>;
-
   constructor(private http: HttpClient) {
     this.updateAvailableSubject = new BehaviorSubject<AppVersion>({
       yourVersion: environment.version,
@@ -29,18 +25,9 @@ export class CordovaService {
     });
     this.updateAvailable = this.updateAvailableSubject.asObservable();
 
-    this.cordovaDeviceSubject = new BehaviorSubject<CordovaDevice>({
-      model: '_window().cordova.device.model',
-      platform: '_window().cordova.device.platform',
-      version: '_window().cordova.device.version',
-      manufacturer: '_window().cordova.device.manufacturer',
-      isVirtual: '_window().cordova.device.isVirtual',
-      serial: '_window().cordova.device.serial',
-      uuid: '_window().cordova.device.uuid',
-    });
-    this.cordovaDevice = this.cordovaDeviceSubject.asObservable();
-
-    // this.cordovaUpdateAvailable();
+    if (this.onCordova) {
+      this.cordovaUpdateAvailable();
+    }
   }
 
   get cordova(): any {
@@ -49,13 +36,12 @@ export class CordovaService {
 
   get onCordova(): Boolean {
     return !!_window().cordova;
-    // return true;
   }
 
   cordovaUpdateAvailable() {
     if (this.onCordova) {
       console.log(`${environment.apiUrl}/cmd/update/cordova/${environment.version}`);
-      this.http.get<any>(`${environment.apiUrl}/cmd/update/cordova/${environment.version}`)
+      return this.http.get<any>(`${environment.apiUrl}/cmd/update/cordova/${environment.version}`)
         .subscribe((data) => {
           if (data['updateAvailable']) {
             console.log('A New Update is available');
@@ -65,18 +51,6 @@ export class CordovaService {
             this.updateAvailableSubject.next(data);
           }
         });
-
-      if (typeof this.cordova !== 'undefined') {
-        this.cordovaDeviceSubject.next({
-          model: this.cordova.device.model,
-          platform: this.cordova.device.platform,
-          version: this.cordova.device.version,
-          manufacturer: this.cordova.device.manufacturer,
-          isVirtual: this.cordova.device.isVirtual,
-          serial: this.cordova.device.serial,
-          uuid: this.cordova.device.uuid,
-        });
-      }
     }
   }
 }
