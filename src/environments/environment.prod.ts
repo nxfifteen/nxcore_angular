f3e158d6c5ef4a2dec71ca72a4234240cd102d6f
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-18 12:18:19 +0100

    refactor(env): Removed prod env file

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  apiUrl: 'https://connect.core.nxfifteen.me.uk',
-  uiUrl: 'https://core.nxfifteen.me.uk',
-  production: true,
-  sentryDns: 'https://0535e5fa521e4246808526cd419c7856@sentry.io/1757010',
-  version: '0.0.6',
-};
