883d01c6794862858b401ce3a1e4f339c8be8dcc
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-18 23:47:09 +0100

    feat(confg): Added service to read host config file
    Issue #1

---
+++ src/app/services/app-config.service.ts
@@ -0,0 +1,39 @@
+/*
+ * This file is part of NxFIFTEEN Fitness Core.
+ *
+ * @link      https://nxfifteen.me.uk/projects/nxcore/
+ * @link      https://gitlab.com/nx-core/frontend/angular
+ * @author    Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @copyright Copyright (c) 2020. Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @license   https://nxfifteen.me.uk/api/license/mit/license.html MIT
+ */
+
+import {Injectable, Injector} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+@Injectable()
+export class AppConfigService {
+  private appConfig;
+
+  constructor(private injector: Injector) {
+  }
+
+  get config() {
+    return this.appConfig;
+  }
+
+  loadAppConfig() {
+    const http = this.injector.get(HttpClient);
+
+    return http.get('/assets/app-config.json')
+      .toPromise()
+      .then(data => {
+        console.log('+++ Okay loaded app-config.json');
+        this.appConfig = data;
+      }).catch(error => {
+        console.log('*** Error loading app-config.json, using environment file instead');
+        this.appConfig = environment;
+      });
+  }
+}
