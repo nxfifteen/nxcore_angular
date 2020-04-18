8686b8b5a37412f69b79a8804392895a18d58bea
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-18 23:44:07 +0100

    refactor(config): Updated default config files with new variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+/*
+ * This file is part of NxFIFTEEN Fitness Core.
+ *
+ * @link      https://nxfifteen.me.uk/projects/nxcore/
+ * @link      https://gitlab.com/nx-core/frontend/angular
+ * @author    Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @copyright Copyright (c) 2020. Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @license   https://nxfifteen.me.uk/api/license/mit/license.html MIT
+ */
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
@@ -9,4 +19,7 @@ export const environment = {
   debugInstance: false,
   sentryDns: '',
   version: '0.0.6',
+  matomoUrl: '',
+  matomoSiteId: -1,
+  matomoSiteIdCordova: -1
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,4 +19,7 @@ export const environment = {
   production: false,
   sentryDns: 'https://0535e5fa521e4246808526cd419c7856@sentry.io/1757010',
   version: '0.0.6',
+  matomoUrl: '',
+  matomoSiteId: -1,
+  matomoSiteIdCordova: -1
 };
