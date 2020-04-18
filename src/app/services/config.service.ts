1a0c9d536497b1f811aa4cec3a3ff3f6fdb986a7
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-19 00:09:39 +0100

    feat(confg): Updated to get user nav bar
    Issue #1

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -8,24 +8,42 @@
  * @license   https://nxfifteen.me.uk/api/license/mit/license.html MIT
  */
 
-import {Injectable} from '@angular/core';
+import {Injectable, Injector} from '@angular/core';
 import {NavData, navItems} from '../_nav';
 import {AuthenticationService} from '../_services';
 import {User} from '../_models';
+import {AppConfigService} from './app-config.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
-  uiSettings: { showNavBar: string | boolean, showAsideBar: string | boolean } = {'showNavBar': true, 'showAsideBar': true};
   navItems: NavData[] = navItems;
   currentUser: User;
+  private userConfig;
+  private readonly apiUrl: string;
 
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(private httpClient: HttpClient,
+              private authenticationService: AuthenticationService,
+              private appConfig: AppConfigService,
+              private injector: Injector) {
+    this.apiUrl = appConfig.config.apiUrl;
     this.loadUserConfig();
   }
 
-  loadUserConfig() {
+  get config() {
+    return this.userConfig;
+  }
+
+  getNavItems() {
+    const URL = `${this.apiUrl}/${this.currentUser.username}/ux/config?key=${this.currentUser.token}`;
+    const response = this.httpClient.get<any>(URL);
+    response.subscribe(data => this.navItems = data);
+    return response;
+  }
+
+  private loadUserConfig() {
     this.authenticationService.currentUser.subscribe((data) => {
       this.currentUser = data;
     });
